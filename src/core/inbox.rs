6bec9244400ad3f7b0e471d2d441e5c8d5ce4a35
Felix Ableitner <me@nutomic.com>
2022-11-14 14:06:40 +0100

    In inbox, verify that http signature digest was verified (ref #1)

--- src/core/inbox.rs
+++ src/core/inbox.rs
@@ -6,14 +6,21 @@ use crate::{
     Error,
     LocalInstance,
 };
-use actix_web::{HttpRequest, HttpResponse};
+use actix_web::{
+    web::{Bytes, Payload},
+    FromRequest,
+    HttpRequest,
+    HttpResponse,
+};
+use anyhow::anyhow;
+use http_signature_normalization_actix::prelude::DigestVerified;
 use serde::de::DeserializeOwned;
-use tracing::log::debug;
+use tracing::{log::debug, warn};
 
 /// Receive an activity and perform some basic checks, including HTTP signature verification.
 pub async fn receive_activity<Activity, ActorT, Datatype>(
     request: HttpRequest,
-    activity: Activity,
+    payload: Payload,
     local_instance: &LocalInstance,
     data: &Data<Datatype>,
 ) -> Result<HttpResponse, <Activity as ActivityHandler>::Error>
@@ -21,10 +28,25 @@ where
     Activity: ActivityHandler<DataType = Datatype> + DeserializeOwned + Send + 'static,
     ActorT: ApubObject<DataType = Datatype> + Actor + Send + 'static,
     for<'de2> <ActorT as ApubObject>::ApubType: serde::Deserialize<'de2>,
-    <Activity as ActivityHandler>::Error:
-        From<anyhow::Error> + From<Error> + From<<ActorT as ApubObject>::Error>,
+    <Activity as ActivityHandler>::Error: From<anyhow::Error>
+        + From<Error>
+        + From<<ActorT as ApubObject>::Error>
+        + From<serde_json::Error>
+        + From<http_signature_normalization_actix::digest::middleware::VerifyError>,
     <ActorT as ApubObject>::Error: From<Error> + From<anyhow::Error>,
 {
+    let mut payload = payload.into_inner();
+    // ensure that payload hash was checked against digest header by middleware
+    DigestVerified::from_request(&request, &mut payload).await?;
+
+    let bytes = Bytes::from_request(&request, &mut payload)
+        .await
+        .map_err(|e| {
+            warn!("{}", e);
+            anyhow!("Failed to parse request body")
+        })?;
+    let activity: Activity = serde_json::from_slice(&bytes)?;
+
     verify_domains_match(activity.id(), activity.actor())?;
     verify_url_valid(activity.id(), &local_instance.settings).await?;
     if local_instance.is_local_url(activity.id()) {
